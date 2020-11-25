ccf7664c38853edb7341244c6ba6345d41af3f77
維菘 <weisung@weisongde-mbp.iiiedu.local>
2020-11-25 21:08:43 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css';
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>     
+      </Router>      
+    </>
+  );
+}
+
+export default App;
