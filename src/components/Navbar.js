0703ace37566d9681383eac8bfd9f65a18f0ea85
維菘 <weisung@weisongde-mbp.iiiedu.local>
2020-12-10 19:26:04 +0800

    finish

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,7 +51,7 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/sign-up" className="nav-links" onClick={closeMobileMenu}>
+                            <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
                                 Sign Up
                             </Link>
                         </li>
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
+import Footer from '../Footer';
 
 export default function Services() {
-    return <h1 className="services">SERVICES</h1>;
+    return (
+    
+        <>
+            <h1 className="services">SERVICES</h1>
+            <Cards />
+            <Footer />
+        </>
+    ) 
 }
